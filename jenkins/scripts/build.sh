2b54338e7b8b2da80a9343d554c08f61b2535767
unknown <jeremimanogi@gmail.com>
2023-06-25 14:13:23 +0700

    uncomment build.sh

--- jenkins/scripts/build.sh
+++ jenkins/scripts/build.sh
@@ -3,5 +3,5 @@
 apt-get purge --auto-remove nodejs npm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.bashrc
-# npm init
-# npm install
+npm init
+npm install
